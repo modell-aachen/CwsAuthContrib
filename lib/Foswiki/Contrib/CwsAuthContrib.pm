4816d8d28de5d11e9173d2a631156f93171eb9d9
Daniel Supplieth <supplieth@modell-aachen.de>
2016-09-02 13:02:09 +0200

    Add missing file.
    Ticket: #11580

---
+++ lib/Foswiki/Contrib/CwsAuthContrib.pm
@@ -0,0 +1,22 @@
+# Module of Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# Copyright (C) 2006-2015 Michael Daum http://michaeldaumconsulting.com
+# Portions Copyright (C) 2006 Spanlink Communications
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version. For
+# more details read LICENSE in the root of this distribution.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# As per the GPL, removal of this notice is prohibited.
+
+use strict;
+use warnings;
+
+1;
+
